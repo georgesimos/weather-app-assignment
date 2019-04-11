b81277816bf3f272c67eea46ca62b2c5b79dc9e0
George <admin@georgesimos.com>
2019-04-11 22:42:45 +0300

    Dynamic pages with hbs templating

--- src/app.js
+++ src/app.js
@@ -7,7 +7,30 @@ const express = require('express')
 const app = express()
 
 publicDirectoryPath = path.join(__dirname, '../public')
+
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        author: 'George Simos'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Page',
+        author: 'George Simos'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help Page',
+        message: 'This is my message'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
