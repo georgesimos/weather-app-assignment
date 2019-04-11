af1910507e74df8feecf63077c82e37dce06bf9b
George <admin@georgesimos.com>
2019-04-11 22:18:24 +0300

    Serving public/static assets

--- src/app.js
+++ src/app.js
@@ -1,24 +1,20 @@
+const path = require('path')
 const express = require('express')
+
+// console.log(__dirname)
+// console.log(path.join(__dirname, '../public'))
 
 const app = express()
 
-// app.com
-// app.com/help
-// app.com/about
-app.get('', (req, res) => {
-    res.send('Hello George Simos!')
-})
+publicDirectoryPath = path.join(__dirname, '../public')
+app.use(express.static(publicDirectoryPath))
 
-app.get('/help', (req, res) => {
-    res.send('Welcome to help section')
-})
-
-app.get('/about', (req, res) => {
-    res.send('Welcome to about section')
-})
 
 app.get('/weather', (req, res) => {
-    res.send('Welcome to weather page')
+    res.send({
+        location: 'Koufonisia',
+        forecast: '14c'
+    })
 })
 
 app.listen(3000, () => {
