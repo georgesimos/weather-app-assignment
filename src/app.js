00dc8c72e70c8e6d52bc051eac48dbf2d7123d8a
George <admin@georgesimos.com>
2019-04-11 22:48:42 +0300

    customizing the Views directory

--- src/app.js
+++ src/app.js
@@ -6,9 +6,15 @@ const express = require('express')
 
 const app = express()
 
+// Define paths for Express config
 publicDirectoryPath = path.join(__dirname, '../public')
+viewPath = path.join(__dirname, '../templates')
 
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs')
+app.set('views', viewPath)
+
+// Setup static directory to serve 
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
