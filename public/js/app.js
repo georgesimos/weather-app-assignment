2ef6f1c80c76ab2429e4d949b5f6a74549021e94
George <admin@georgesimos.com>
2019-04-14 17:51:15 +0300

    Adding a search form to get user address

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,25 @@
-console.log('Client side JavaScript working')
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
 
-fetch('http://localhost:3000/weather?address=koufonisia')
-    .then((res) => {
-        res.json().then(data => {
-            if (data.error) console.log(data.error);
-            console.log(data)
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = search.value
+    messageOne.removeAttribute('style');
+    messageOne.textContent = 'loading...';
+    messageTwo.textContent = '';
+    fetch('http://localhost:3000/weather?address=' + location)
+        .then((res) => {
+            res.json().then(data => {
+                if (data.error) {
+                    messageOne.style.color = 'red';
+                    messageOne.textContent = data.error;
+                } else {
+                    messageOne.textContent = data.location;
+                    messageTwo.textContent = data.summary;
+                }
+            })
         })
-    })
+})
